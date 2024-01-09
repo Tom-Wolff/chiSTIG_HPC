91a4cfed3e4e6f9caa23c125ed75d6323169d77a
Tom Wolff <tom.wolff@northwestern.edu>
2024-01-09 14:26:14 -0600

    Changed script for further testing

--- R/workflow_02-model_calibration.R
+++ R/workflow_02-model_calibration.R
@@ -10,7 +10,7 @@ library("EpiModelHIV")
 # Settings ---------------------------------------------------------------------
 source("./R/utils-0_project_settings.R")
 context <- "hpc"
-max_cores <- 1
+max_cores <- 30
 
 source("./R/utils-chistig_basic_inputs.R") # make `path_to_est`, `param` and `init`
 source("./R/utils-hpc_configs.R") # creates `hpc_configs`
@@ -39,41 +39,14 @@ control <- control_msm(
   nsteps              = calibration_end,
   nsims               = 1,
   ncores              = 1,
-  # cumulative.edgelist = TRUE,
+  cumulative.edgelist = TRUE,
   truncate.el.cuml    = 0,
   .tracker.list       = calibration_trackers,
-  verbose             = FALSE,
-  # raw.output          = TRUE,
-  initialize.FUN =              chiSTIGmodules::initialize_msm_chi,
-  aging.FUN =                   chiSTIGmodules::aging_msm_chi,
-  departure.FUN =               chiSTIGmodules::departure_msm_chi,
-  arrival.FUN =                 chiSTIGmodules::arrival_msm_chi,
-  # venues.FUN =                chiSTIGmodules:::venues_msm_chi,
-  partident.FUN =               chiSTIGmodules::partident_msm_chi,
-  hivtest.FUN =                 chiSTIGmodules::hivtest_msm_chi,
-  hivtx.FUN =                   chiSTIGmodules::hivtx_msm_chi,
-  hivprogress.FUN =             chiSTIGmodules::hivprogress_msm_chi,
-  hivvl.FUN =                   chiSTIGmodules::hivvl_msm_chi,
-  resim_nets.FUN =              chiSTIGmodules::simnet_msm_chi,
-  acts.FUN =                    chiSTIGmodules::acts_msm_chi,
-  condoms.FUN =                 chiSTIGmodules::condoms_msm_chi,
-  position.FUN =                chiSTIGmodules::position_msm_chi,
-  prep.FUN =                    chiSTIGmodules::prep_msm_chi,
-  hivtrans.FUN =                chiSTIGmodules::hivtrans_msm_chi,
-  exotrans.FUN =                chiSTIGmodules:::exotrans_msm_chi,
-  stitrans.FUN =                chiSTIGmodules::stitrans_msm_chi,
-  stirecov.FUN =                chiSTIGmodules::stirecov_msm_chi,
-  stitx.FUN =                   chiSTIGmodules::stitx_msm_chi,
-  prev.FUN =                    chiSTIGmodules::prevalence_msm_chi,
-  cleanup.FUN =                 chiSTIGmodules::cleanup_msm_chi,
-
-  module.order = c("aging.FUN", "departure.FUN", "arrival.FUN", # "venues.FUN",
-                   "partident.FUN", "hivtest.FUN", "hivtx.FUN", "hivprogress.FUN",
-                   "hivvl.FUN", "resim_nets.FUN", "acts.FUN", "condoms.FUN",
-                   "position.FUN", "prep.FUN", "hivtrans.FUN", "exotrans.FUN",
-                   "stitrans.FUN", "stirecov.FUN", "stitx.FUN", "prev.FUN", "cleanup.FUN")
+  .checkpoint.dir     = "./temp/cp_calib",
+  .checkpoint.clear   = TRUE,
+  .checkpoint.steps   = 15 * 52,
+  verbose             = FALSE
 )
-
 
 # insert test values here
 n_scenarios <- 2
@@ -91,10 +64,10 @@ wf <- add_workflow_step(
   wf_summary = wf,
   step_tmpl = step_tmpl_netsim_scenarios(
     path_to_est, param, init, control,
-    scenarios_list = NULL,
+    scenarios_list = scenarios_list,
     output_dir = "./data/intermediate/calibration",
     libraries = "EpiModelHIV",
-    n_rep = 2,
+    n_rep = 120,
     n_cores = max_cores,
     save_pattern = "simple",
     max_array_size = 999,
@@ -104,32 +77,30 @@ wf <- add_workflow_step(
     "mail-type" = "FAIL,TIME_LIMIT",
     "cpus-per-task" = max_cores,
     "time" = "04:00:00",
-    "mem-per-cpu" = "5G"
+    "mem" = 0
   )
 )
 
 # Process calibrations
 #
 # produce a data frame with the calibration targets for each scenario
-# wf <- add_workflow_step(
-#   wf_summary = wf,
-#   step_tmpl = step_tmpl_do_call_script(
-#     r_script = "./R/11-calibration_process.R",
-#     args = list(
-#       context = "hpc",
-#       ncores = 15
-#     ),
-#     setup_lines = hpc_configs$r_loader
-#   ),
-#   sbatch_opts = list(
-#     "cpus-per-task" = 15,
-#     "time" = "04:00:00",
-#     "mem-per-cpu" = "4G",
-#     "mail-type" = "END"
-#   )
-# )
-
-
+wf <- add_workflow_step(
+  wf_summary = wf,
+  step_tmpl = step_tmpl_do_call_script(
+    r_script = "./R/11-calibration_process.R",
+    args = list(
+      context = "hpc",
+      ncores = 15
+    ),
+    setup_lines = hpc_configs$r_loader
+  ),
+  sbatch_opts = list(
+    "cpus-per-task" = 15,
+    "time" = "04:00:00",
+    "mem-per-cpu" = "4G",
+    "mail-type" = "END"
+  )
+)
 
 # Send the workflow folder to the <HPC> and run it
 #
